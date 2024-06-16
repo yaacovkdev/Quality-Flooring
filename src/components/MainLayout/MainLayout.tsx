ee7bab7c6950a1afde68a8541b551299932ab650
yaacovkdev <yaacov.kochatkov@gmail.com>
2024-06-16 00:49:11 -0700

    showcase id route background flooring white

--- src/components/MainLayout/MainLayout.tsx
+++ src/components/MainLayout/MainLayout.tsx
@@ -1,16 +1,33 @@
+"use client";
+
 import "./MainLayout.scss";
 import Header from "../Header/Header";
 import Footer from "../Footer/Footer";
+import { useParams, usePathname } from "next/navigation";
+import { useEffect, useState } from "react";
 
 export default function MainLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  //make sure the page
+  const query = useParams();
+  const path = usePathname();
+
+  const [boxClassname, setBoxClassName] = useState("main-layout-box");
+
+  useEffect(() => {
+    if (path.match(/^(\/show\/)/g) && query.id) {
+      setBoxClassName(boxClassname + " main-layout-box--showcase-item");
+    }
+    console.log(query.id);
+  }, []);
+
   return (
-    <div className="main-layout-box">
+    <div className={boxClassname}>
       <Header />
-        <div className="content">{children}</div>
+      <div className="content">{children}</div>
       <Footer />
     </div>
   );
